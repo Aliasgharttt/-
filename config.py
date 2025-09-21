c45799d47483f1d6d07d6c594cd546789463252a
Aliasgharttt <jddn4019@gmail.com>
2025-09-21 12:39:50 +0330

    config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-TOKEN="8418297859:AAHH89g2QwsEsmWnmM09bm4hRGfw55FRo18"
+import os
+
+# توکن ربات از محیط گرفته میشه (نه مستقیم تو کد!)
+TOKEN = os.environ.get("8418297859:AAHH89g2QwsEsmWnmM09bm4hRGfw55FRo18")
+
+# Render خودش PORT رو ست می‌کنه
+PORT = int(os.environ.get("PORT", 1000))
